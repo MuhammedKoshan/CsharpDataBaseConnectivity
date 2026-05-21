2b5b346a40c050cdc6d69a9d8478287905539bf9
Muhammed Koşan <muhammedkoshan29@gmail.com>
2026-05-21 17:04:36 +0300

    Update and rename AddingData to AddingData.cs

---
+++ Insert_Add_Data/AddingData.cs
@@ -0,0 +1,77 @@
+
+    static void AddNewContactAndGetID(stContact newContact)
+    {
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+
+        string query = @"INSERT INTO Contacts (FirstName,LastName,Email,Phone,Address,CountryID)
+                      VALUES (@FirstName,@LastName,@Email,@Phone,@Address,@CountryID);
+               SELECT SCOPE_IDENTITY();";
+
+
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@FirstName", newContact.FirstName);
+        command.Parameters.AddWithValue("@LastName", newContact.LastName);
+        command.Parameters.AddWithValue("@Email", newContact.Email);
+        command.Parameters.AddWithValue("@Phone", newContact.Phone);
+        command.Parameters.AddWithValue("@Address", newContact.Address);
+        command.Parameters.AddWithValue("@CountryID", newContact.CountryID);
+
+
+
+        try
+        {
+            connection.Open();
+
+
+            object result = command.ExecuteScalar();
+
+            if(result !=null && int.TryParse(result.ToString (), out int insertedID))
+            {
+                Console.WriteLine($"Newly inserted ID: {insertedID}");
+                
+            }
+            else
+            {
+                Console.WriteLine("Failed to retrieve the inserted ID.");
+
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        connection.Close();
+
+
+
+    }
+
+
+
+    public static void Main()
+    {
+
+        stContact contact = new stContact
+        {
+            FirstName = "Laila",
+            LastName = "Maher",
+            Email = "m@example.com",
+            Phone = "1234567890",
+            Address = "123 Main Street",
+            CountryID = 1
+
+        };
+        AddNewContactAndGetID(contact);
+
+
+
+
+
+        Console.ReadKey();
+    }
+
+}
